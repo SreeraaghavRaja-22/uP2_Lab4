4de35b7622991f6218aca502588e820ded2640c2
SreeraaghavRaja-22 <rajas@ufl.edu>
2025-10-20 02:36:49 -0400

    11: finished the stuff I wanted to finish

---
+++ lab4_files/lab4_files/lab4_1.c
@@ -0,0 +1,64 @@
+// Lab 3, uP2 Fall 2025
+// Created: 2023-07-31
+// Updated: 2025-07-09
+// Lab 3 is intended to introduce you to RTOS concepts. In this, you will
+// - configure the systick function
+// - write asm functions for context switching
+// - write semaphore functions
+// - write scheduler functions to add threads / run scheduling algorithms
+// - write critical section assembly functions
+
+/************************************Includes***************************************/
+
+#include "G8RTOS/G8RTOS.h"
+#include "./MultimodDrivers/multimod.h"
+
+#include "./threads.h"
+
+/************************************Includes***************************************/
+
+/*************************************Defines***************************************/
+/*************************************Defines***************************************/
+
+/********************************Public Variables***********************************/
+//extern uint32_t SystemTime;
+
+/********************************Public Variables***********************************/
+/********************************Public Functions***********************************/
+
+
+/********************************Public Functions***********************************/
+
+/************************************MAIN*******************************************/
+
+// Be sure to add in your source files from previous labs into "MultimodDrivers/src/"!
+// If you made any modifications to the corresponding header files, be sure to update
+// those, too.
+int main(void) {
+
+    // sysclock
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+
+    // Initializes the necessary peripherals.
+    Multimod_Init();
+   
+    
+
+    // Add threads, initialize semaphores here!
+    G8RTOS_InitSemaphore(&sem_UART, UART_Resources);
+    G8RTOS_InitSemaphore(&sem_I2CA, I2C_Resources);
+
+    G8RTOS_Init();
+    G8RTOS_AddThread(Thread0, 0, "Thread 0");
+    G8RTOS_AddThread(Thread1, 1, "Thread 1");
+    G8RTOS_AddThread(Thread2, 2, "Thread 2");
+    G8RTOS_AddThread(Thread3, 3, "Thread 3");
+    G8RTOS_AddThread(Thread4, 4, "Thread 4");
+    G8RTOS_Launch();
+
+    // spin - the RTOS will take over now
+    while (1);
+}
+
+/************************************MAIN*******************************************/
